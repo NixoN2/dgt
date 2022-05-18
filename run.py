11bb4da1852f5822f900bc410e7e37048a75ff5a
Andrew <mythtics2001@mail.ru>
2022-05-18 19:11:45 +0300

    added random node

--- run.py
+++ run.py
@@ -20,22 +20,33 @@ def create_command(functions,prefix,wallets):
 cluster = sys.argv[1]
 node = sys.argv[2]
 n = int(sys.argv[3])
+max_node = int(sys.argv[4])
 
-prefix = f"python3 transaction_time.py {cluster} {node}"
+nodes = []
+for i in range(1,max_node+1):
+    nodes.append(i)
 
-wallet_number = random.randint(1, n)
+prefixes = []
+
+for i in nodes:
+    prefixes.append(f"python3 transaction_time.py {cluster} {i}")
+
+#prefix = f"python3 transaction_time.py {cluster} {node}"
+
+wallet_number = random.randint(1, n) % 15
 
 wallets = []
 
 for i in range(wallet_number):
     wallets.append(f"wal{i}")
 
-for wallet in wallets:
-    os.system(f"{prefix} -f set -w {wallet} -c {random.randint(100,10000)}")
+for prefix in prefixes: 
+    for wallet in wallets:
+        os.system(f"{prefix} -f set -w {wallet} -c {random.randint(100,10000)}")
 
 functions = ["inc","trans","dec"]
 
 for i in range(n):
-    os.system(create_command(functions,prefix,wallets))
+    os.system(create_command(functions,random.choice(prefixes),wallets))
 
 
