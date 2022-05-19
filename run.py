8bf68cb3880a5e8096103a98df8b9fda81b9b4e4
Andrew <mythtics2001@mail.ru>
2022-05-20 00:00:37 +0300

    fix

--- run.py
+++ run.py
@@ -40,7 +40,7 @@ for i in clusters:
 
 #prefix = f"python3 transaction_time.py {cluster} {node}"
 
-wallet_number = random.randint(1, n) % 6
+wallet_number = random.randint(3, n) % 6
 
 wallets = []
 
